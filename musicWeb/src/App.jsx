ea5b1f46df38809f22cd55b876311d9e5281a313
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-15 16:41:04 +0530

    create sildebar component

--- musicWeb/src/App.jsx
+++ musicWeb/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
+import React from "react";
+import Slidebar from "./components/Slidebar";
 
 const App = () => {
   return (
-    <div className='h-screen bg-black'>
-      <div className='h-[90%] flex '>
-
+    <div className="h-screen bg-black">
+      <div className="h-[90%] flex ">
+        <Slidebar />
       </div>
-      
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ musicWeb/src/components/Slidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Slidebar = () => {
+  return (
+    <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
+      <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+            
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slidebar;
