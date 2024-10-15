6489502e8366e6fc7a4cf3b192b38183accd0499
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-15 16:09:42 +0530

    add project assets

---
+++ musicWeb/src/assets/admin-assets/assets.js
@@ -0,0 +1,22 @@
+import logo from './logo.png'
+import logo_small from './logo_small.png'
+import upload_area from './upload_area.png'
+import upload_added from './upload_added.png'
+import add_album from './add_album.png'
+import add_song from './add_song.png'
+import song_icon from './song_icon.png'
+import album_icon from './album_icon.png'
+import upload_song from './upload_song.png'
+
+export const assets ={
+    logo,
+    logo_small,
+    upload_area,
+    upload_added,
+    add_album,
+    add_song,
+    song_icon,
+    album_icon,
+    upload_song
+}
+
