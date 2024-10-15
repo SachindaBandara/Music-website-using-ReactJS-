d122c38777e7ede39876fa2ee4d83f53ea7909bd
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-15 17:33:42 +0530

    add

--- musicWeb/src/components/Slidebar.jsx
+++ musicWeb/src/components/Slidebar.jsx
@@ -34,6 +34,13 @@ const Slidebar = () => {
             Create playlist
           </button>
         </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4 mt-4">
+          <h1>Let's findsome podcasts to follow</h1>
+          <p className="font-light">We'll keep you update on new episodes</p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Browse Podcasts
+          </button>
+        </div>
       </div>
     </div>
   );
