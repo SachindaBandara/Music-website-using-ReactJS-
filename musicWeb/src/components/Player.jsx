6007c1ce05ebfd02c1bd8e7aafe0fbdb3cb21cc4
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-15 17:47:55 +0530

    add player menu

--- musicWeb/src/components/Player.jsx
+++ musicWeb/src/components/Player.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { songsData } from "../assets/frontend-assets/assets";
+import { assets, songsData } from "../assets/frontend-assets/assets";
 
 const Player = () => {
   return (
@@ -8,7 +8,16 @@ const Player = () => {
         <img className="w-12" src={songsData[0].image} alt="" />
         <div>
           <p>{songsData[0].name}</p>
-          <p>{songsData[0].desc.slice(0,12)}</p>
+          <p>{songsData[0].desc.slice(0, 12)}</p>
+        </div>
+      </div>
+      <div className="flex flex-col items-center gap-1 m-auto">
+        <div className="flex gap-5">
+          <img className="w-4 cursor-pointer" src={assets.shuffle_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.play_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
       </div>
     </div>
