1bf6c5c97b246d943548edd9ac9a6f0f49c9883d
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-15 17:29:30 +0530

    add

--- musicWeb/src/components/Slidebar.jsx
+++ musicWeb/src/components/Slidebar.jsx
@@ -24,8 +24,15 @@ const Slidebar = () => {
 
           <div className="flex items-center gap-3">
             <img className="w-5" src={assets.arrow_icon} alt="" />
-            <img className="w-5" src={assets.plus_icon_icon} alt="" />
+            <img className="w-5" src={assets.plus_icon} alt="" />
           </div>
+        </div>
+        <div className="p-4 bg-[#242424] m-2 rounded font-semibold flex flex-col items-start justify-start gap-1 pl-4">
+          <h1>Create your first playlist</h1>
+          <p className="font-light">it's easy we will help you</p>
+          <button className="px-4 py-1.5 bg-white text-[15px] text-black rounded-full mt-4">
+            Create playlist
+          </button>
         </div>
       </div>
     </div>
