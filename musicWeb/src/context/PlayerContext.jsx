be070e54dbcc336dd93c6819a6d68d97679ae0fc
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-16 18:37:08 +0530

    create music player progress bar

--- musicWeb/src/context/PlayerContext.jsx
+++ musicWeb/src/context/PlayerContext.jsx
@@ -31,9 +31,19 @@ const PlayerContextProvider = (props) => {
     setPlayerStatus(false);
   };
 
+  const playWithId = async (id) => {
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    playerStatus(true);
+  };
+
   useEffect(() => {
     setTimeout(() => {
       audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width =
+          Math.floor(
+            (audioRef.current.currentTime / audioRef.current.duration) * 100
+          ) + "%";
         setTime({
           currentTime: {
             second: Math.floor(audioRef.current.currentTime % 60),
@@ -60,6 +70,7 @@ const PlayerContextProvider = (props) => {
     setTime,
     play,
     pause,
+    playWithId
   };
 
   return (
