2f586218138acd9dca35dfb2c702e7f244cb1a1e
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-16 11:45:45 +0530

    created frontend of the muisc album

--- musicWeb/src/components/DisplayHome.jsx
+++ musicWeb/src/components/DisplayHome.jsx
@@ -2,22 +2,42 @@ import React from "react";
 import NavBar from "./NavBar";
 import { albumsData } from "../assets/frontend-assets/assets";
 import AlubumItem from "./AlubumItem";
+import { songsData } from "../assets/frontend-assets/assets";
+import SongItem from "./SongItem";
 
 const DisplayHome = () => {
   return (
     <>
       <NavBar />
 
-      <div className="my-5 font-bold text-2xl">
-        {albumsData.map((item, index) => (
-          <AlubumItem
-            key={index}
-            name={item.name}
-            desc={item.desc}
-            id={item.id}
-            image={item.image}
-          />
-        ))}
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Featured Charts</h1>
+        <div className="flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlubumItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <h1 className="my-5 font-bold text-2xl">Today's Biggest Hits</h1>
+        <div className="flex overflow-auto">
+          {songsData.map((item, index) => (
+            <SongItem
+              key={index}
+              name={item.name}
+              desc={item.desc}
+              id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
