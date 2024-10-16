9e5c6fc50f916d2910a89366f5413a74f9f2d20d
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-16 22:58:57 +0530

    create song model

---
+++ backend/src/models/songModel.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const songSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  desc: { type: String, required: true },
+  album: { type: String, required: true },
+  image: { type: String, required: true },
+  file: { type: String, required: true },
+  duration: { type: String, required: true },
+});
+
+const songModel = mongoose.models.song || mongoose.model("song", songSchema);
+
+export default songModel;
