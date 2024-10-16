8f3d2f64f31d9927d07f554bea0e4b324edc2fac
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-16 09:25:09 +0530

    create player items

--- musicWeb/src/components/Player.jsx
+++ musicWeb/src/components/Player.jsx
@@ -28,7 +28,21 @@ const Player = () => {
           <div className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer">
             <hr className="h-1 border-none w-0 bg-green-800 rounded-full" />
           </div>
+          <p>3:20</p>
         </div>
+      </div>
+      <div className="hidden lg:flex items-center gap-2 opacity-75">
+        <img className="w-4" src={assets.plays_icon} alt="" />
+        <img className="w-4" src={assets.mic_icon} alt="" />
+        <img className="w-4" src={assets.queue_icon} alt="" />
+        <img className="w-4" src={assets.speaker_icon} alt="" />
+        <img className="w-4" src={assets.volume_icon} alt="" />
+        <div className="w-20 bg-slate-50 h-1 rounded">
+
+        </div>
+        <img className="w-4" src={assets.mini_player_icon} alt="" />
+        <img className="w-4" src={assets.zoom_icon} alt="" />
+
       </div>
     </div>
   );
