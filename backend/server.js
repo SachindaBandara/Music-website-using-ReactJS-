3aeaa296b91323afeb13d63efb3b90ca725b222e
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-16 22:48:44 +0530

    connect cloudinary into the project

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import songRouter from "./src/routes/songRoute.js";
+import connectCloudinary from "./src/config/cloudinary.js";
 
 // app config
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
-
+connectCloudinary();
 // middlewares
 app.use(express.json());
 app.use(cors());
