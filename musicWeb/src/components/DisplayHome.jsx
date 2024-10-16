3300e12bfbe67fd66c1d7e39386bc928a94e7800
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-16 11:29:38 +0530

    create music album part

--- musicWeb/src/components/DisplayHome.jsx
+++ musicWeb/src/components/DisplayHome.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from "react";
+import NavBar from "./NavBar";
+import { albumsData } from "../assets/frontend-assets/assets";
+import AlubumItem from "./AlubumItem";
 
 const DisplayHome = () => {
   return (
     <>
       <NavBar />
-    </>
-  )
-}
 
-export default DisplayHome
+      <div className="my-5 font-bold text-2xl">
+        {albumsData.map((item, index) => (
+          <AlubumItem
+            key={index}
+            name={item.name}
+            desc={item.desc}
+            id={item.id}
+            image={item.image}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default DisplayHome;
