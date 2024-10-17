c3f789586d5073953b0429b45bb740821e23ad3a
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-17 22:39:20 +0530

    create list song

--- admin/src/pages/ListSong.jsx
+++ admin/src/pages/ListSong.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { url } from "../App";
+import { toast } from "react-toastify";
 
 const ListSong = () => {
+  const [data, setData] = useState([]);
+
+  const fecthSongs = async () => {
+    try {
+      const response = await axios.get(`${url}/api/song/list`);
+
+      if (response.data.success) {
+        setData(response.data.songs);
+      }
+    } catch (error) {
+      toast.error("Error occured");
+    }
+  };
+
+  useEffect(() => {
+    fecthSongs();
+  }, []);
   return (
     <div>
-      
+      <p>All Songs List</p>
+      <br />
+      <div>
+        <div className="sm:grid hidden grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5 bg-gray-100">
+          <b>Image</b>
+          <b>Name</b>
+          <b>Album</b>
+          <b>Duration</b>
+          <b>Action</b>
+        </div>
+        {data.map((item, index) => {
+          return (
+            <div
+              key={index}
+              className="grid grid-cols-[1fr_1fr_1fr] sm:grid-cols-[0.5fr_1fr_2fr_1fr_0.5fr] items-center gap-2.5 p-3 border border-gray-300 text-sm mr-5"
+            >
+              <img className="w-12" src={item.image} alt="" />
+              <p>{item.name}</p>
+              <p>{item.album}</p>
+              <p>{item.duration}</p>
+              <p>x</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ListSong
+export default ListSong;
