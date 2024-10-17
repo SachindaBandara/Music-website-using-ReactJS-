4f8df74a4c91049941df112704c36d9b75de8859
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-17 15:37:30 +0530

    created list album logic

--- backend/src/controllers/albumController.js
+++ backend/src/controllers/albumController.js
@@ -28,7 +28,14 @@ const addAlbum = async (req, res) => {
   }
 };
 
-const listAlbum = async (req, res) => {};
+const listAlbum = async (req, res) => {
+  try {
+    const allAlbums = await albumModel.find({});
+    res.json({ success: true, albums: allAlbums });
+  } catch (error) {
+    res.json({ success: false });
+  }
+};
 
 const removeAlbum = async (req, res) => {};
 
