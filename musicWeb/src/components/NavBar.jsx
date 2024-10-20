3d7b2436dbbdaa6f0c7fcf119e0422c944630e2c
SachindaUB <jmsubandara@std.appsc.sab.ac.lk>
2024-10-20 22:27:19 +0530

    add style format

--- musicWeb/src/components/NavBar.jsx
+++ musicWeb/src/components/NavBar.jsx
@@ -9,28 +9,28 @@ const NavBar = () => {
     <>
       <div className="w-full flex justify-between items-center font-semibold">
         <div className="flex items-center gap-2">
-          <img onClick={()=> navigate(-1)}
+          <img onClick={() => navigate(-1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_left}
             alt=""
           />
-           <img onClick={()=> navigate(1)}
+          <img onClick={() => navigate(1)}
             className="w-8 bg-black p-2 rounded-2xl cursor-pointer"
             src={assets.arrow_right}
             alt=""
           />
         </div>
         <div className="flex items-center gap-4">
-            <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">Explore Premium</p>
-            <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">Install App</p>
-            <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">S</p>
+          <p className="bg-white text-black text-[15px] px-4 py-1 rounded-2xl hidden md:block cursor-pointer">Explore Premium</p>
+          <p className="bg-black py-1 px-3 rounded-2xl text-[15px] cursor-pointer">Install App</p>
+          <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center">S</p>
         </div>
       </div>
       <div className="flex items-center gap-2 mt-4">
-            <p className="bg-white text-black px-4 py-1 rounded-2xl">All</p>
-            <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
-            <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Podcasts</p>
-        </div>
+        <p className="bg-white text-black px-4 py-1 rounded-2xl">All</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Music</p>
+        <p className="bg-black px-4 py-1 rounded-2xl cursor-pointer">Podcasts</p>
+      </div>
     </>
   );
 };
--- musicWeb/src/components/Slidebar.jsx
+++ musicWeb/src/components/Slidebar.jsx
@@ -9,7 +9,7 @@ const Slidebar = () => {
   return (
     <div className="w-[25%] h-full p-2 flex-col gap-2 text-white hidden lg:flex">
       <div className="bg-[#121212] h-[15%] rounded flex flex-col justify-around">
-        <div onClick={()=> navigate('/')} className="flex items-center gap-3 pl-8 cursor-pointer">
+        <div onClick={() => navigate('/')} className="flex items-center gap-3 pl-8 cursor-pointer">
           <img className="w-6" src={assets.home_icon} alt="" />
           <p className="font-bold">Home</p>
         </div>
